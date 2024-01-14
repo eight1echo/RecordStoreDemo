883c83d7be03427bc26fcaf0c2e504be9e341f70
Andrew Olsvik <a.ols@outlook.com>
2024-01-13 23:48:05 -0400

    Add Inventory Queries GetOnHandHistory GetPriceHistory

---
+++ src/RecordStoreDemo/Features/Inventory/Products/Queries/GetOnHandHistory/GetOnHandHistoryEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace RecordStoreDemo.Features.Inventory.Products.Queries.GetOnHandHistory;
+
+public class GetOnHandHistoryEndpoint(RecordStoreDbContext _context) : EndpointBaseAsync
+    .WithRequest<Guid>
+    .WithResult<ActionResult<List<OnHandHistoryModel>>>
+{
+    [HttpGet("api/inventory/{id}/onhand")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [SwaggerOperation(
+        Summary = "Get On Hand History",
+        OperationId = "OnHandHistory_Get",
+        Tags = new[] { "Inventory" })]
+    public override async Task<ActionResult<List<OnHandHistoryModel>>> HandleAsync(
+      Guid id,
+      CancellationToken cancellationToken = default)
+    {
+        var onHandHistory = await _context.OnHandHistory
+            .Where(i => i.ProductId == id)
+            .Select(i => new OnHandHistoryModel
+            {
+                Date = i.DateCreated.ToShortDateString(),
+                NewOnHand = i.NewOnHand,
+                QuantityChange = i.QuantityChange,
+                Reason = i.Reason,
+            }).ToListAsync(cancellationToken);
+
+        return Ok(onHandHistory);
+    }
+}
---
+++ src/RecordStoreDemo/Features/Inventory/Products/Queries/GetPriceHistory/GetPriceHistoryEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace RecordStoreDemo.Features.Inventory.Products.Queries.GetPriceHistory;
+
+public class GetPriceHistoryEndpoint(RecordStoreDbContext _context) : EndpointBaseAsync
+    .WithRequest<Guid>
+    .WithResult<ActionResult<List<PriceHistoryModel>>>
+{
+    [HttpGet("api/inventory/{id}/price")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [SwaggerOperation(
+        Summary = "Get Price History",
+        OperationId = "PriceHistory_Get",
+        Tags = new[] { "Inventory" })]
+    public override async Task<ActionResult<List<PriceHistoryModel>>> HandleAsync(
+      Guid id,
+      CancellationToken cancellationToken = default)
+    {
+        var priceHistory = await _context.PriceHistory
+            .Where(p => p.ProductId == id)
+            .Select(p => new PriceHistoryModel
+            {
+                Date = p.DateCreated.ToShortDateString(),
+                NewPrice = p.NewPrice.Value,
+                OldPrice = p.OldPrice.Value,
+                Reason = p.Reason
+            }).ToListAsync(cancellationToken);
+
+        return Ok(priceHistory);
+    }
+}
