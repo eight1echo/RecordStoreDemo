4cc434ecc37d28001df0e0b819a62c59e436ae36
Andrew Olsvik <a.ols@outlook.com>
2024-01-13 23:19:40 -0400

    Database Migration - Receiving

--- src/RecordStoreDemo/Persistence/Migrations/20240114031802_Receiving.Designer.cs
+++ src/RecordStoreDemo/Persistence/Migrations/20240114031802_Receiving.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecordStoreDemo.Persistence;
 
@@ -11,9 +12,11 @@ using RecordStoreDemo.Persistence;
 namespace RecordStoreDemo.Persistence.Migrations
 {
     [DbContext(typeof(RecordStoreDbContext))]
-    partial class RecordStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240114031802_Receiving")]
+    partial class Receiving
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/RecordStoreDemo/Persistence/Migrations/20240114031802_Receiving.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStoreDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Receiving : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Receives",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VendorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DateSubmitted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receives", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceiveItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReceiveId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CatalogProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InventoryProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiveItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReceiveItem_CatalogProducts_CatalogProductId",
+                        column: x => x.CatalogProductId,
+                        principalTable: "CatalogProducts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ReceiveItem_InventoryProducts_InventoryProductId",
+                        column: x => x.InventoryProductId,
+                        principalTable: "InventoryProducts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ReceiveItem_Receives_ReceiveId",
+                        column: x => x.ReceiveId,
+                        principalTable: "Receives",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiveItem_CatalogProductId",
+                table: "ReceiveItem",
+                column: "CatalogProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiveItem_InventoryProductId",
+                table: "ReceiveItem",
+                column: "InventoryProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiveItem_ReceiveId",
+                table: "ReceiveItem",
+                column: "ReceiveId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReceiveItem");
+
+            migrationBuilder.DropTable(
+                name: "Receives");
+        }
+    }
+}
