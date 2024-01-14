cf4819ff8acec6f6cc89d63c260fadfbe9b52fd3
Andrew Olsvik <a.ols@outlook.com>
2024-01-13 23:05:55 -0400

    Database Migration - InventoryProduct

---
+++ src/RecordStoreDemo/Persistence/Migrations/20240114030421_InventoryProduct.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStoreDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InventoryProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InventoryProducts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CatalogProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Format = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OnHand = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    StreetDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    UPC = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InventoryProducts_CatalogProducts_CatalogProductId",
+                        column: x => x.CatalogProductId,
+                        principalTable: "CatalogProducts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OnHandHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NewOnHand = table.Column<int>(type: "int", nullable: false),
+                    QuantityChange = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InventoryProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OnHandHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OnHandHistory_InventoryProducts_InventoryProductId",
+                        column: x => x.InventoryProductId,
+                        principalTable: "InventoryProducts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PriceHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OldPrice = table.Column<decimal>(name: "Old Price", type: "decimal(8,2)", nullable: false),
+                    NewPrice = table.Column<decimal>(name: "New Price", type: "decimal(8,2)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InventoryProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PriceHistory_InventoryProducts_InventoryProductId",
+                        column: x => x.InventoryProductId,
+                        principalTable: "InventoryProducts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryProducts_CatalogProductId",
+                table: "InventoryProducts",
+                column: "CatalogProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OnHandHistory_InventoryProductId",
+                table: "OnHandHistory",
+                column: "InventoryProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PriceHistory_InventoryProductId",
+                table: "PriceHistory",
+                column: "InventoryProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OnHandHistory");
+
+            migrationBuilder.DropTable(
+                name: "PriceHistory");
+
+            migrationBuilder.DropTable(
+                name: "InventoryProducts");
+        }
+    }
+}
