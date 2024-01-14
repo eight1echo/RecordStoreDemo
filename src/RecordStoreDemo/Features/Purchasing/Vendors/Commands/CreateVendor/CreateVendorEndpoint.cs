14b8749aaaea907fe4d15644195e85e365d6b757
Andrew Olsvik <a.ols@outlook.com>
2024-01-13 23:41:23 -0400

    Add Vendor Command CreateVendor

---
+++ src/RecordStoreDemo/Features/Purchasing/Vendors/Commands/CreateVendor/CreateVendorEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace RecordStoreDemo.Features.Purchasing.Vendors.Commands.CreateVendor;
+
+public class CreateVendorEndpoint(IVendorRepository _vendorRepo) : EndpointBaseAsync
+    .WithRequest<CreateVendorRequest>
+    .WithActionResult
+{
+    [HttpPost("api/purchasing/vendors")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [SwaggerOperation(
+        Summary = "Create Vendor",
+        OperationId = "Vendor_Create",
+        Tags = new[] { "Vendors" })]
+    public override async Task<ActionResult> HandleAsync(
+      CreateVendorRequest request,
+      CancellationToken cancellationToken = default)
+    {
+        var vendor = new Vendor(request.Name);
+
+        await _vendorRepo.Add(vendor);
+
+        var result = new VendorModel()
+        {
+            Id = vendor.Id,
+            Name = vendor.Name,
+        };
+
+        return CreatedAtRoute("GetVendor", new { id = result.Id }, result);
+    }
+}
---
+++ src/RecordStoreDemo/Features/Purchasing/Vendors/Commands/CreateVendor/CreateVendorRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RecordStoreDemo.Features.Purchasing.Vendors.Commands.CreateVendor;
+public class CreateVendorRequest
+{
+    [Required]
+    [Length(2, 40)]
+    public string Name { get; set; } = string.Empty;
+}
