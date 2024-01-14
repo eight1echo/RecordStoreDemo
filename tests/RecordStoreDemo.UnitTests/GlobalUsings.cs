0a76f531c7010a65d52bbdd2d98a14dca880034c
Andrew Olsvik <a.ols@outlook.com>
2024-01-13 23:07:56 -0400

    Add InventoryProduct Unit Tests

--- tests/RecordStoreDemo.UnitTests/GlobalUsings.cs
+++ tests/RecordStoreDemo.UnitTests/GlobalUsings.cs
@@ -1,5 +1,8 @@
 global using Xunit;
 
+global using RecordStoreDemo.Common.Exceptions;
+
+global using RecordStoreDemo.Features.Inventory.Products;
 global using RecordStoreDemo.Features.Purchasing.PurchaseOrders;
 global using RecordStoreDemo.Features.Purchasing.Catalogs;
 global using RecordStoreDemo.Features.Purchasing.Vendors;
---
+++ tests/RecordStoreDemo.UnitTests/Inventory/InventoryProductTests.cs
@@ -0,0 +1,64 @@
+﻿namespace RecordStoreDemo.UnitTests.Inventory;
+public class InventoryProductTests
+{
+    private readonly InventoryProduct _testProduct;
+
+    public InventoryProductTests()
+    {
+        _testProduct = new InventoryProduct(new Guid(), "TestArtist", "Media", "CD", "Metal", 12.99m, DateTime.Now, "TestProduct", "0123456789") { Id = new Guid() };
+    }
+
+    [Fact]
+    public void CanAdjustOnHand()
+    {
+        _testProduct.OnHandAdjustment(1, "Testing Inventory");
+
+        Assert.True(_testProduct.OnHand == 1);
+    }
+
+    [Fact]
+    public void CanChangePrice()
+    {
+        _testProduct.PriceAdjustment(99.99m, "Testing Price Change");
+
+        Assert.True(_testProduct.Price.Value == 99.99m);
+    }
+
+    [Fact]
+    public void ShouldSaveInventoryHistory()
+    {
+        var quantityChange = 1;
+        var reason = "Testing Price Change";
+
+        _testProduct.OnHandAdjustment(quantityChange, reason);
+
+        var change = _testProduct.OnHandHistory.FirstOrDefault(c => c.Reason == reason);
+
+        Assert.NotNull(change);
+        Assert.True(change.QuantityChange == quantityChange);
+        Assert.True(change.Reason == reason);
+    }
+
+    [Fact]
+    public void ShouldSavePriceHistory()
+    {
+        var oldPrice = _testProduct.Price.Value;
+        var newPrice = 99.99m;
+        var reason = "Testing Price Change";
+
+        _testProduct.PriceAdjustment(newPrice, reason);
+
+        var change = _testProduct.PriceHistory.FirstOrDefault(c => c.Reason == reason);
+
+        Assert.NotNull(change);
+        Assert.True(change.NewPrice.Value == newPrice);
+        Assert.True(change.OldPrice.Value == oldPrice);
+        Assert.True(change.Reason == reason);
+    }
+
+    [Fact]
+    public void ShouldNotChangeInventoryToBelowZero()
+    {
+        Assert.Throws<InventoryQuantityException>(() => _testProduct.OnHandAdjustment(-11, "Testing Inventory"));
+    }
+}
