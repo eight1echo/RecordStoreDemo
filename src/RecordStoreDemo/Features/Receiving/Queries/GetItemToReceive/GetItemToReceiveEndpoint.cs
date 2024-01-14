d18cdcd1fafa216b38f8db018e4e4491bedb00d6
Andrew Olsvik <a.ols@outlook.com>
2024-01-14 13:49:09 -0400

    Add Receiving Queries GetItemToReceive GetPendingReceive

---
+++ src/RecordStoreDemo/Features/Receiving/Queries/GetItemToReceive/GetItemToReceiveEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using RecordStoreDemo.Features.Inventory.Products.Commands.UpdateProductDetails;
+using RecordStoreDemo.Features.Receiving.Commands.AddItemToReceive;
+
+namespace RecordStoreDemo.Features.Receiving.Queries.GetItemToReceive;
+
+public class GetItemToReceiveEndpoint(RecordStoreDbContext _context) : EndpointBaseAsync
+    .WithRequest<string>
+    .WithResult<ActionResult<AddItemToReceiveRequest>>
+{
+    [HttpGet("api/receiving/items/{upc}", Name = "GetReceiveItem")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(
+        Summary = "Get Item To Receive",
+        OperationId = "Receive_GetItem",
+        Tags = new[] { "Receiving" })]
+    public override async Task<ActionResult<AddItemToReceiveRequest>> HandleAsync(
+      string upc,
+      CancellationToken cancellationToken = default)
+    {
+        var addItemRequest = await _context.InventoryProducts
+            .Where(p => p.UPC.Value == upc)
+            .Select(p => new AddItemToReceiveRequest
+            {
+                CatalogProductId = p.CatalogProductId,
+                InventoryProductId = p.Id,
+                Quantity = 1,
+                UPC = p.UPC.Value,
+                UpdateRequest = new UpdateProductDetailsRequest
+                {
+                    InventoryProductId = p.Id,
+                    Artist = p.Artist,
+                    Department = p.Category.Department,
+                    Format = p.Category.Format,
+                    Genre = p.Genre,
+                    StreetDate = p.StreetDate,
+                    Title = p.Title,
+                    UPC = p.UPC.Value
+                }
+            }).FirstOrDefaultAsync(cancellationToken);
+
+        if (addItemRequest is not null)
+            return Ok(addItemRequest);
+
+        return NotFound();
+    }
+}
---
+++ src/RecordStoreDemo/Features/Receiving/Queries/GetPendingReceive/GetPendingReceiveEndpoint.cs
@@ -0,0 +1,53 @@
+﻿namespace RecordStoreDemo.Features.Receiving.Queries.GetPendingReceive;
+
+public class GetPendingReceiveEndpoint(RecordStoreDbContext _context) : EndpointBaseAsync
+    .WithRequest<Guid>
+    .WithResult<ActionResult<ReceiveModel>>
+{
+    [HttpGet("api/receiving/{vendorId}", Name = "GetReceive")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(
+        Summary = "Get Pending Receive",
+        OperationId = "Receive_Get",
+        Tags = new[] { "Receiving" })]
+    public override async Task<ActionResult<ReceiveModel>> HandleAsync(
+      Guid vendorId,
+      CancellationToken cancellationToken = default)
+    {
+        var receive = await _context.Receives
+            .Where(r => r.VendorId == vendorId && r.Status == ReceiveStatus.Pending)
+            .Select(r => new ReceiveModel
+            {
+                Id = r.Id,
+                VendorId = vendorId,
+                DateCreated = r.DateCreated,
+                Status = r.Status,
+
+                Items = r.Items.Select(i => new ReceiveItemModel
+                {
+                    Id = i.Id,
+                    Cost = i.CatalogProduct.Cost.Value,
+                    ProductId = i.InventoryProductId,
+                    Quantity = i.Quantity,
+
+                    Product = new InventoryProductModel
+                    {
+                        Artist = i.InventoryProduct.Artist,
+                        Department = i.InventoryProduct.Category.Department,
+                        Format = i.InventoryProduct.Category.Format,
+                        Genre = i.InventoryProduct.Genre,
+                        Price = i.InventoryProduct.Price.Value,
+                        Title = i.InventoryProduct.Title,
+                        UPC = i.InventoryProduct.UPC.Value
+                    }
+                }).ToList()
+
+            }).FirstOrDefaultAsync(cancellationToken);
+
+        if (receive is not null)
+            return Ok(receive);
+
+        return NotFound();
+    }
+}
