d6dd9fa2570b7e4256ceaa4c97d311f8ae6d9219
Andrew Olsvik <a.ols@outlook.com>
2024-01-13 23:20:27 -0400

    Add Receiving Unit Tests

--- tests/RecordStoreDemo.UnitTests/GlobalUsings.cs
+++ tests/RecordStoreDemo.UnitTests/GlobalUsings.cs
@@ -6,3 +6,4 @@ global using RecordStoreDemo.Features.Inventory.Products;
 global using RecordStoreDemo.Features.Purchasing.PurchaseOrders;
 global using RecordStoreDemo.Features.Purchasing.Catalogs;
 global using RecordStoreDemo.Features.Purchasing.Vendors;
+global using RecordStoreDemo.Features.Receiving;
---
+++ tests/RecordStoreDemo.UnitTests/Receiving/ReceiveTests.cs
@@ -0,0 +1,32 @@
+﻿namespace RecordStoreDemo.UnitTests.Receiving;
+public class ReceiveTests
+{
+    private readonly CatalogProduct _testCatalogProduct;
+    private readonly InventoryProduct _testInventoryProduct;
+    private readonly Receive _testReceive;
+    private readonly Vendor _testVendor;
+
+    public ReceiveTests()
+    {
+        _testCatalogProduct = new CatalogProduct("TestVendor", "TestArtist", 10.0m, "TestDescription", "CD", "TestLabel", "TestSKU", DateTime.Now, "TestTitle", "1234567890") { Id = Guid.NewGuid() };
+        _testInventoryProduct = new InventoryProduct(new Guid(), "TestArtist", "Media", "CD", "Metal", 12.99m, DateTime.Now, "TestProduct", "0123456789") { Id = new Guid() };
+        _testVendor = new Vendor("TestVendor") { Id = Guid.NewGuid() };
+        _testReceive = new Receive(_testVendor.Id) { Id = Guid.NewGuid() };
+    }
+
+    [Fact]
+    public void ShouldNotSubmitIfAlreadySubmitted()
+    {
+        var item = new ReceiveItem(_testReceive.Id, _testInventoryProduct.Id, _testCatalogProduct.Id, 1) { CatalogProduct = _testCatalogProduct, InventoryProduct = _testInventoryProduct };
+        _testReceive.AddItem(item);
+        _testReceive.Submit();
+
+        Assert.Throws<InvalidOperationException>(_testReceive.Submit);
+    }
+
+    [Fact]
+    public void ShouldNotSubmitIfNoItems()
+    {
+        Assert.Throws<InvalidOperationException>(_testReceive.Submit);
+    }
+}
