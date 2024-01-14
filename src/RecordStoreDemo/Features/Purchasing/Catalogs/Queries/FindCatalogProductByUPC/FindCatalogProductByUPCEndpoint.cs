60f55e5fe3ff26ae3edd0935646841d075468cf6
Andrew Olsvik <a.ols@outlook.com>
2024-01-14 14:11:00 -0400

    Add Purchasing Queries FindCatalogProductByUPC FIndCatalogProducts

---
+++ src/RecordStoreDemo/Features/Purchasing/Catalogs/Queries/FindCatalogProductByUPC/FindCatalogProductByUPCEndpoint.cs
@@ -0,0 +1,45 @@
+﻿namespace RecordStoreDemo.Features.Purchasing.Catalogs.Queries.GetCatalogProductByUPC;
+
+public class FindCatalogProductByUPCEndpoint(RecordStoreDbContext _context) : EndpointBaseAsync
+    .WithRequest<string>
+    .WithResult<ActionResult<CatalogProductModel>>
+{
+    [HttpGet("api/purchasing/catalogs/find/{upc}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(
+        Summary = "Find Catalog Product By UPC",
+        OperationId = "CatalogProduct_FindByUPC",
+        Tags = new[] { "Catalogs" })]
+    public override async Task<ActionResult<CatalogProductModel>> HandleAsync(
+      string upc,
+      CancellationToken cancellationToken = default)
+    {
+        var product = await _context.CatalogProducts
+            .Where(p => p.UPC.Value == upc)
+            .Select(p => new CatalogProductModel
+            {
+                Id = p.Id,
+                VendorId = p.VendorId,
+                VendorName = p.VendorName,
+
+                Artist = p.Artist,
+                Cost = p.Cost.Value,
+                Description = p.Description,
+                Format = p.Format,
+                Label = p.Label,
+                SKU = p.SKU,
+                StreetDate = p.StreetDate.ToShortDateString(),
+                Title = p.Title,
+                UPC = p.UPC.Value,
+
+                CartQuantity = p.CartQuantity,
+                OrderedQuantity = p.OrderedQuantity
+            }).FirstOrDefaultAsync(cancellationToken);
+
+        if (product is not null)
+            return Ok(product);
+
+        return NotFound();
+    }
+}
---
+++ src/RecordStoreDemo/Features/Purchasing/Catalogs/Queries/FindCatalogProducts/FindCatalogProductsEndpoint.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq.Expressions;
+
+namespace RecordStoreDemo.Features.Purchasing.Catalogs.Queries.FindCatalogProducts;
+
+public class FindCatalogProductsEndpoint(RecordStoreDbContext _context) : EndpointBaseAsync
+    .WithRequest<ProductQueryRequest>
+    .WithResult<ActionResult<List<CatalogProductModel>>>
+{
+    [HttpGet("api/purchasing/catalogs/find")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [SwaggerOperation(
+        Summary = "Find Catalog Products",
+        OperationId = "CatalogProducts_Find",
+        Tags = new[] { "Catalogs" })]
+    public override async Task<ActionResult<List<CatalogProductModel>>> HandleAsync(
+      [FromQuery] ProductQueryRequest request,
+      CancellationToken cancellationToken = default)
+    {
+        Expression<Func<CatalogProduct, bool>> filter;
+
+        var queryable = _context.CatalogProducts.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.UPC))
+        {
+            filter = vp => vp.UPC.Value == request.UPC;
+            queryable = queryable.Where(filter);
+        }
+        else
+        {
+            request.Artist ??= string.Empty;
+            request.Title ??= string.Empty;
+
+            var split = request.Artist.Split(' ').ToList();
+
+            foreach (var s in split)
+            {
+                queryable = queryable.Where(vp => vp.Artist.Contains(s) && vp.Title.Contains(request.Title));
+            }
+        }
+
+        var products = await queryable
+            .OrderBy(vp => vp.Artist)
+            .Select(x => new CatalogProductModel()
+            {
+                Id = x.Id,
+
+                VendorId = x.VendorId,
+                VendorName = x.VendorName,
+
+                OrderedQuantity = x.OrderedQuantity,
+                CartQuantity = x.CartQuantity,
+
+                Artist = x.Artist,
+                Cost = x.Cost.Value,
+                Description = x.Description,
+                Format = x.Format,
+                Label = x.Label,
+                SKU = x.SKU,
+                StreetDate = x.StreetDate.ToString("yyyy-MM-dd"),
+                Title = x.Title,
+                UPC = x.UPC.Value
+            }).ToListAsync(cancellationToken);
+
+        return products;
+    }
+}
