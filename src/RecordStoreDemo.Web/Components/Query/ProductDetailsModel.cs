6d1c10d8e054bf3fd9d4af63e6fc71698edf439a
Andrew Olsvik <a.ols@outlook.com>
2024-01-15 13:06:29 -0400

    Add Query Web UI Components

---
+++ src/RecordStoreDemo.Web/Components/Query/ProductDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using RecordStoreDemo.Features.Inventory.Products;
+using RecordStoreDemo.Features.Purchasing.Catalogs;
+using RecordStoreDemo.Features.Webstore.Products;
+
+namespace RecordStoreDemo.Web.Components.Query;
+public class ProductDetailsModel
+{
+    public CatalogProductModel? CatalogProduct { get; set; }
+    public InventoryProductModel? InventoryProduct { get; set; }
+    public WebstoreProductModel? WebstoreProduct { get; set; }
+
+    public string UPC { get; set; } = string.Empty;
+    public bool IsConsignment { get; set; }
+}
