cd004873e428751a0b69a6f04f29b5d1ee822319
Andrew Olsvik <a.ols@outlook.com>
2024-01-15 13:06:52 -0400

    Add Webstore Web UI Components

---
+++ src/RecordStoreDemo.Web/SelectionHelpers.cs
@@ -0,0 +1,33 @@
+﻿using RecordStoreDemo.Features.Webstore.Products;
+
+namespace RecordStoreDemo.Web;
+
+public static class SelectionHelpers
+{
+    public static bool IsGenreSelected(WebstoreProductModel product, string selectedGenre)
+    {
+        if (product.Genres.Contains(selectedGenre))
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    public static bool IsStyleSelected(WebstoreProductModel product, string selectedStyle)
+    {
+        if (product.Styles.Contains(selectedStyle))
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    public static bool IsTagSelected(WebstoreProductModel product, string selectedTag)
+    {
+        if (product.Tags.Contains(selectedTag))
+        {
+            return true;
+        }
+        else return false;
+    }
+}
