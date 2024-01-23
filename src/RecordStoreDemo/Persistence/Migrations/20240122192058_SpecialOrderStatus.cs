800e5ea5e4a19f0b8a74aa595654ed7fc27810ae
Andrew Olsvik <a.ols@outlook.com>
2024-01-23 18:33:08 -0400

    Database Migration - SpecialOrders

---
+++ src/RecordStoreDemo/Persistence/Migrations/20240122192058_SpecialOrderStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStoreDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecialOrderStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "SpecialOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SpecialOrders");
+        }
+    }
+}
