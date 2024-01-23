0992f815881a47e855c4bbda0cd871ff831162a2
Andrew Olsvik <a.ols@outlook.com>
2024-01-23 18:34:18 -0400

    Add CustomerProfileCommand CreateCustomerProfile

---
+++ src/RecordStoreDemo/Features/Customers/Profiles/Commands/CreateCustomer/CreateCustomerProfileEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace RecordStoreDemo.Features.Customers.Profiles.Commands.CreateCustomer;
+public class CreateCustomerProfileEndpoint(ICustomerProfileRepository _customerRepo) : EndpointBaseAsync
+    .WithRequest<CreateCustomerProfileRequest>
+    .WithActionResult
+{
+    [HttpPost("api/customers")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [SwaggerOperation(
+        Summary = "Create Customer Profile",
+        OperationId = "CustomerProfiles_Create",
+        Tags = new[] { "Customers" })]
+    public override async Task<ActionResult> HandleAsync(
+      CreateCustomerProfileRequest request,
+      CancellationToken cancellationToken = default)
+    {
+        var customer = new CustomerProfile(request.Name, request.Contact);
+
+        await _customerRepo.Add(customer);
+
+        var result = new CustomerProfileModel()
+        {
+            Id = customer.Id,
+            Name = customer.Name,
+            Contact = customer.GetContact()
+        };
+
+        return CreatedAtRoute("GetVendor", new { id = result.Id }, result);
+    }
+}
---
+++ src/RecordStoreDemo/Features/Customers/Profiles/Commands/CreateCustomer/CreateCustomerProfileRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordStoreDemo.Features.Customers.Profiles.Commands.CreateCustomer;
+public class CreateCustomerProfileRequest
+{
+    [Required]
+    [Length(2, 40)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [EmailOrPhoneNumber]
+    public string Contact { get; set; } = null!;
+}
