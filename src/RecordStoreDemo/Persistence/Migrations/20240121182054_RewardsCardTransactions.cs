372169c63ff563e59ee591c629f0aa402cc99fa8
Andrew Olsvik <a.ols@outlook.com>
2024-01-23 18:32:55 -0400

    Database Migration - RewardsCard

---
+++ src/RecordStoreDemo/Persistence/Migrations/20240121182054_RewardsCardTransactions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStoreDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RewardsCardTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RewardsTransaction",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PointsChange = table.Column<int>(type: "int", nullable: false),
+                    RewardsCardId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RewardsTransaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RewardsTransaction_RewardsCards_RewardsCardId",
+                        column: x => x.RewardsCardId,
+                        principalTable: "RewardsCards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RewardsTransaction_RewardsCardId",
+                table: "RewardsTransaction",
+                column: "RewardsCardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RewardsTransaction");
+        }
+    }
+}
---
+++ src/RecordStoreDemo/Persistence/Migrations/20240121183218_RewardsCardTransactionsContext.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStoreDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RewardsCardTransactionsContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RewardsTransaction_RewardsCards_RewardsCardId",
+                table: "RewardsTransaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RewardsTransaction",
+                table: "RewardsTransaction");
+
+            migrationBuilder.RenameTable(
+                name: "RewardsTransaction",
+                newName: "RewardsTransactions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RewardsTransaction_RewardsCardId",
+                table: "RewardsTransactions",
+                newName: "IX_RewardsTransactions_RewardsCardId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RewardsTransactions",
+                table: "RewardsTransactions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RewardsTransactions_RewardsCards_RewardsCardId",
+                table: "RewardsTransactions",
+                column: "RewardsCardId",
+                principalTable: "RewardsCards",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RewardsTransactions_RewardsCards_RewardsCardId",
+                table: "RewardsTransactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RewardsTransactions",
+                table: "RewardsTransactions");
+
+            migrationBuilder.RenameTable(
+                name: "RewardsTransactions",
+                newName: "RewardsTransaction");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RewardsTransactions_RewardsCardId",
+                table: "RewardsTransaction",
+                newName: "IX_RewardsTransaction_RewardsCardId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RewardsTransaction",
+                table: "RewardsTransaction",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RewardsTransaction_RewardsCards_RewardsCardId",
+                table: "RewardsTransaction",
+                column: "RewardsCardId",
+                principalTable: "RewardsCards",
+                principalColumn: "Id");
+        }
+    }
+}
