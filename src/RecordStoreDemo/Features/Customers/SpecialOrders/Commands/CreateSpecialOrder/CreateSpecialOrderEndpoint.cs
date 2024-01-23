4e4806f3b467120b1f2b8571f3af3067adeac66a
Andrew Olsvik <a.ols@outlook.com>
2024-01-23 18:35:33 -0400

    Add SpecialOrder Command CreateSpecialOrder

---
+++ src/RecordStoreDemo/Features/Customers/SpecialOrders/Commands/CreateSpecialOrder/CreateSpecialOrderEndpoint.cs
@@ -0,0 +1,57 @@
+﻿namespace RecordStoreDemo.Features.Customers.SpecialOrders.Commands.CreateSpecialOrder;
+public class CreateSpecialOrderEndpoint(
+    ICustomerProfileRepository _customerRepo, 
+    IInventoryProductRepository _inventoryRepo, 
+    IPurchaseOrderRepository _purchaseOrder) : EndpointBaseAsync
+    .WithRequest<CreateSpecialOrderRequest>
+    .WithActionResult
+{
+    [HttpPost("api/specialorders/")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [SwaggerOperation(
+        Summary = "Create Customer Special Order",
+        OperationId = "SpecialOrder_Create",
+        Tags = new[] { "SpecialOrders" })]
+    public override async Task<ActionResult> HandleAsync(
+      CreateSpecialOrderRequest request,
+      CancellationToken cancellationToken = default)
+    {
+        var customer = await _customerRepo.GetCustomer(request.CustomerProfileId);
+        var product = await _inventoryRepo.GetProduct(request.InventoryProductId);
+        var purchaseOrder = await _purchaseOrder.GetPendingPurchaseOrderByVendorId(product.CatalogProduct.VendorId);
+
+        var existing = purchaseOrder.Items
+            .Where(i => i.CatalogProductId == product.CatalogProductId)
+            .FirstOrDefault();
+
+        if (existing is null)
+        {
+            purchaseOrder.AddItem(product.CatalogProduct, request.Quantity);
+        }
+        else
+        {
+            existing = purchaseOrder.UpdateItem(product.CatalogProductId, existing.Quantity + request.Quantity);
+        }
+        
+        var specialOrder = customer.AddSpecialOrder(request.InventoryProductId);
+
+        await _customerRepo.Update(customer);
+
+        var result = new SpecialOrderModel
+        {
+            Id = specialOrder.Id,
+            DateOrdered = specialOrder.DateOrdered,
+            Price = specialOrder.Product.Price.Value,
+            Product = $"{product.Artist}/{product.Title} [{product.Category.Format}]",
+            UPC = specialOrder.Product.UPC.Value,
+
+            CustomerProfile = new CustomerProfileModel
+            {
+                Name = customer.Name,
+                Contact = customer.GetContact()
+            }
+        };      
+
+        return CreatedAtRoute("GetSpecialOrder", new { id = result.Id }, result);
+    }
+}
---
+++ src/RecordStoreDemo/Features/Customers/SpecialOrders/Commands/CreateSpecialOrder/CreateSpecialOrderRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RecordStoreDemo.Features.Customers.SpecialOrders.Commands.CreateSpecialOrder;
+public class CreateSpecialOrderRequest
+{
+    public Guid CustomerProfileId { get; set; }
+    public Guid InventoryProductId { get; set; }
+    public int Quantity { get; set; } = 1;
+}
