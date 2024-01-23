62ccc9a08304d1773acc333e60d172fa2217a0c9
Andrew Olsvik <a.ols@outlook.com>
2024-01-23 18:30:38 -0400

    Add RewardsCard Entities RewardsCard RewardsTransaction

---
+++ src/RecordStoreDemo/Features/Customers/Rewards/RewardsCard.cs
@@ -0,0 +1,36 @@
+﻿namespace RecordStoreDemo.Features.Customers.Rewards;
+public class RewardsCard : BaseEntity
+{
+    public RewardsCard()
+    {
+        Number = string.Empty;
+    }
+    public RewardsCard(string number) 
+    {
+        Number = number;
+        Points = 0;
+    }
+    public string Number { get; private set; }
+    public int Points { get; private set; }
+
+    private readonly List<RewardsTransaction> _transactions = [];
+    public virtual ICollection<RewardsTransaction> Transactions => _transactions.AsReadOnly();
+
+    public void SetCardNumber(string number)
+    {
+        Number = Guard.Against.InvalidCardNumber(number, nameof(Number));
+    }
+    public RewardsTransaction AddTransaction(string cardNumber, int pointsChange)
+    {
+        if (cardNumber != Number)
+        {
+            throw new InvalidOperationException("Card Numbers do not match.");
+        }
+
+        var transaction = new RewardsTransaction(pointsChange);
+        _transactions.Add(transaction);
+        Points += pointsChange;
+
+        return transaction;
+    }
+}
---
+++ src/RecordStoreDemo/Features/Customers/Rewards/RewardsTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordStoreDemo.Features.Customers.Rewards;
+public class RewardsTransaction : BaseEntity
+{
+    public RewardsTransaction(int pointsChange)
+    {
+        Date = DateTime.Now;
+        PointsChange = pointsChange;
+    }
+    public DateTime Date { get; private set; }
+    public int PointsChange { get; private set; }
+}
