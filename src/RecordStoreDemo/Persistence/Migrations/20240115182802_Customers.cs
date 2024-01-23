6c98fcc36d43120033a739ea1ed5b95d4c8ee02b
Andrew Olsvik <a.ols@outlook.com>
2024-01-23 18:32:39 -0400

    Database Migrations - CustomerProfile

---
+++ src/RecordStoreDemo/Persistence/Migrations/20240115182802_Customers.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordStoreDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Customers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RewardsCards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RewardsCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RewardsCardId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerProfiles_RewardsCards_RewardsCardId",
+                        column: x => x.RewardsCardId,
+                        principalTable: "RewardsCards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpecialOrders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InventoryProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateOrdered = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateReceived = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Contacted = table.Column<bool>(type: "bit", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecialOrders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SpecialOrders_CustomerProfiles_CustomerProfileId",
+                        column: x => x.CustomerProfileId,
+                        principalTable: "CustomerProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SpecialOrders_InventoryProducts_InventoryProductId",
+                        column: x => x.InventoryProductId,
+                        principalTable: "InventoryProducts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerProfiles_RewardsCardId",
+                table: "CustomerProfiles",
+                column: "RewardsCardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecialOrders_CustomerProfileId",
+                table: "SpecialOrders",
+                column: "CustomerProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecialOrders_InventoryProductId",
+                table: "SpecialOrders",
+                column: "InventoryProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpecialOrders");
+
+            migrationBuilder.DropTable(
+                name: "CustomerProfiles");
+
+            migrationBuilder.DropTable(
+                name: "RewardsCards");
+        }
+    }
+}
